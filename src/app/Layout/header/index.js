80e2dc0b8961a0d5c95d32bd0f1b1c3d03f63bac
Michaelceke <ekemicheal@yahoo.com>
2021-07-20 14:13:54 +0100

    header fixed

--- src/app/Layout/header/index.js
+++ src/app/Layout/header/index.js
@@ -1,34 +1,34 @@
 import React from "react";
 import { Layout, Input } from "antd";
 import { Logo } from "./../../../components/Logo/index";
+import { SearchOutlined } from '@ant-design/icons';
 
 import styles from "./header.module.scss";
+
 const { Header } = Layout;
 const { Search } = Input;
 
-const onSearch = value => console.log(value);
+const onSearch = (value) => console.log(value);
 const PageHeader = () => {
   return (
     <>
-      <Header className={styles.header} theme={'light'}>
+      <Header className={styles.header} theme={"light"} >
+          <div className={styles.headerleft}>
+              <Logo />
+          </div>
 
-            <div className={styles.headerleft}>
 
-            </div>
+        <div className={styles.headerright}>
+          <div className={styles.search}>
+              <Input placeholder={"Find food and drinks"} addonAfter={<SearchOutlined />} />
+          </div>
 
-              <div className={styles.headerright}>
-                       <Search
-                           placeholder="input search text"
-                           onSearch={onSearch}
-                           enterButton />
-
-                       <nav>
-                           <a href={"#"}>Home</a>
-                           <a href={"#"}>Cart</a>
-                           <a href={"#"}>Order history</a>
-                       </nav>
-              </div>
-
+          <nav className={styles.links}>
+            <a href={"#"}>Home</a>
+            <a href={"#"}>Cart</a>
+            <a href={"#"}>Order history</a>
+          </nav>
+        </div>
       </Header>
     </>
   );
